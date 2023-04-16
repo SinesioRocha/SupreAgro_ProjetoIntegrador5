7a6bd00a47e48ce64230f767152e417f06703736
SinesioRocha <266780@unifio.edu.br>
2023-04-16 18:17:09 -0300

    Criacao do Cabecalho / Menu

---
+++ src/Navbar.js
@@ -0,0 +1,40 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function CollapsibleExample() {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="secondary" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home"><img
+            src="http://supreagro.com.br/wp-content/uploads/2020/08/cropped-Logo-1-Color-2-2048x1030.png"
+            width="150vh"
+            style={{
+              marginTop: "10px",
+              marginBottom: "10px",
+              marginRight: "20px"
+            }}
+          /></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Clientes</Nav.Link>
+            <Nav.Link href="#pricing">Colaboradores</Nav.Link>
+            <Nav.Link href="#pricing">Produtos</Nav.Link>
+            <NavDropdown title="Agenda" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Agenda Do Dia</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Novo Agendamento</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Pedido" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Lista Pedido</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Novo Pedido</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default CollapsibleExample;
